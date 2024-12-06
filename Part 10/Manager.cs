cd28a9c47f979f84a551fa8a4b4c9071abbb6f8e
NixInteractive <nixinteractive@yahoo.com>
2024-12-05 22:47:34 -0500

    Update Manager.cs

--- Part 10/Manager.cs
+++ Part 10/Manager.cs
@@ -2,25 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using BreakInfinity;
 
 public class Manager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI nameDisp;
     [SerializeField] private TextMeshProUGUI descDisp;
-    [SerializeField] private TextMeshProUGUI quantityDisp;
-
-    private CurrencyManager currencyManager;
 
     public string managerName;
     public string managerDescription;
 
-    public BigDouble quantity;
-
     // Start is called before the first frame update
     void Start()
     {
-        currencyManager = FindObjectOfType<CurrencyManager>();
+        
     }
 
     // Update is called once per frame
@@ -33,6 +27,5 @@ public class Manager : MonoBehaviour
     {
         nameDisp.text = managerName;
         descDisp.text = managerDescription;
-        quantityDisp.text = currencyManager.SciNotToUSName(quantity);
     }
 }
